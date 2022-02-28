088ef26d64e542feaf90298ffcf7247ecac856ce
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2022-02-28 22:55:28 +0100

    App built with Javascript and fetching data from API

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const btn = document.getElementById("button")
+const adviceId = document.getElementById("advice-id")
+const adviceTxt = document.getElementById("advice")
+
+btn.addEventListener("click", generateAdvice)
+
+function generateAdvice() {
+    fetch("https://api.adviceslip.com/advice")
+        .then(response => response.json())
+        .then(data => {
+            adviceId.textContent = `Advice #${data.slip.id}`
+            adviceTxt.textContent = `"${data.slip.advice}"`
+        })
+        .catch(error => alert("Try later"))
+}
